63b5dc44cdcb6d5018e5115c25e5e08503b499fd
noam.dst <dos-santos.noam@epitech.eu>
2020-05-15 04:42:11 +0200

    test chat

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+    socket.on('chat message', (data) => {
+        console.log('message : ' + data);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 http.listen(3000, () => {
